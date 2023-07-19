5dcbc55cbd301ddbaba2f1b69e7507d3b693fbe4
Ivan <kryamky@yandex.ru>
2023-07-19 10:30:09 +0300

    Client validation form

--- src/components/FormCallback/FormCallback.jsx
+++ src/components/FormCallback/FormCallback.jsx
@@ -1,15 +1,26 @@
-import { Button, Form, Input, Row } from 'antd'
-import React from 'react'
+import { Button, Form, Input, Row, message } from 'antd';
+import React, { useState } from 'react';
 import { rules } from '../../utils/rules';
 
 
 
 function FormCallback({ onCloseModal }) {
 	const [form] = Form.useForm()
+	const [messageApi, contextHolder] = message.useMessage();
+	const [loading, setLoading] = useState(false)
+
+	const success = () => {
+		messageApi.open({
+			type: 'success',
+			content: 'Ваше сообщение успешно отправлено',
+		});
+	};
 
 	const onFinish = async (values) => {
 
+
 		try {
+			setLoading(true)
 			const formData = new FormData();
 			formData.append('action', 'callback');
 			for (const key in values) {
@@ -31,36 +42,47 @@ function FormCallback({ onCloseModal }) {
 			form.setFields(json.fields)
 			if (json.valid) {
 				console.log('success!!!!');
-				alert('Форма успешно отправлена')
-				form.resetFields()
-				onCloseModal()
+
+				setTimeout(() => {
+					success();
+					form.resetFields()
+					setLoading(false)
+				}, 1000);
+				setTimeout(() => {
+					onCloseModal()
+				}, 1500);
 			}
 		} catch (error) {
 			console.log(error)
+			setLoading(false)
 		}
+
 	}
 
 
 	return (
-		<Form onFinish={onFinish} layout="vertical" form={form}>
-			<Form.Item label="Имя" name="name" rules={[rules.required()]}>
-				<Input />
-			</Form.Item>
-			<Form.Item label="Фамилия" name="lastname" rules={[rules.required()]}>
-				<Input />
-			</Form.Item>
-			<Form.Item label="Телефон или почта" name="telEmail" rules={[rules.required()]}>
-				<Input />
-			</Form.Item>
-			<Form.Item label="Сообщение" name="message" rules={[rules.required()]}>
-				<Input.TextArea />
-			</Form.Item>
-			<Row justify="center">
-				<Form.Item>
-					<Button type="primary" htmlType="submit"> Submit </Button>
+		<>
+			{contextHolder}
+			<Form onFinish={onFinish} layout="vertical" form={form}>
+				<Form.Item label="Имя" name="name" rules={[rules.required(), rules.text()]}>
+					<Input />
 				</Form.Item>
-			</Row>
-		</Form>
+				<Form.Item label="Фамилия" name="lastname" rules={[rules.required(), rules.text()]}>
+					<Input />
+				</Form.Item>
+				<Form.Item label="Телефон или почта" name="telEmail" rules={[rules.required(), rules.telOrEmail()]}>
+					<Input />
+				</Form.Item>
+				<Form.Item label="Сообщение" name="message" rules={[rules.required()]}>
+					<Input.TextArea />
+				</Form.Item>
+				<Row justify="center">
+					<Form.Item>
+						<Button type="primary" htmlType="submit" loading={loading}> Submit </Button>
+					</Form.Item>
+				</Row>
+			</Form>
+		</>
 	)
 }
 
--- src/utils/rules.js
+++ src/utils/rules.js
@@ -1,3 +1,23 @@
 export const rules = {
-	required: (message = 'Обязательное поле') => ({ required: true, message })
+	required: (message = 'Обязательное поле') => ({ required: true, message }),
+	text: (message = 'Должно быть менее 15 символов') => ({ max: 15, message }),
+	telOrEmail: (message = 'Некорректый телефон или почта') => ({
+		validator(_, value) {
+			console.log(value)
+			if (value === '' || value === undefined) {
+				return Promise.resolve();
+			}
+			const telPattern = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+			const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+			if (telPattern.test(value)) {
+				return Promise.resolve();
+			}
+			else if (emailPattern.test(value)) {
+				return Promise.resolve();
+			}
+			else {
+				return Promise.reject(new Error(message));
+			}
+		},
+	})
 }
